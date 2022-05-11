93fc41e5917461b57b7e82fee145d9a98cee2c90
German <german.rodriguezgu@gmail.com>
2022-05-10 22:02:33 -0500

    :zap: :hammer: Creacion de mutations y actions en el store de vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,18 +1,34 @@
 import { createStore } from "vuex";
+import getRandomInt from "@/helpers/getRandomInt";
 
 /**
  * * Se hace el export default para que sea accesible desde el index.js
  * * Para utilizar el store, hay que incluirlo como un plugin en el main.js
  * * state - 
- * * mutations - 
- * * actions -
+ * * mutations - metodos que modifican el state sincrono
+ * * actions - metodos que pueden ser asincronos
  */
 export default createStore({
 
-    state:{
-        count: 1
+    state: {
+        count: 1,
+        lastMutation: 'none',
+        isLoading: false,
+    },
+    mutations: {
+        increment(state) {
+            state.count++;
+        },
+        incrementBy(state, value) {
+            state.count += value;
+            state.lastMutation = 'incrementBy' + value;
+        }
+    },
+    actions: {
+        async incrementRandomInt(context) {
+            const randomInt = await getRandomInt();
+            context.commit('incrementBy', randomInt);
+        }
     }
-
-
 
 })
