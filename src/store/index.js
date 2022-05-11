e3ce52416951ce7993eff406bcf3764f10666857
German <german.rodriguezgu@gmail.com>
2022-05-10 21:22:18 -0500

    :tada: :zap: :sparkles: Bases de vue con vuex

---
+++ src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "vuex";
+
+/**
+ * * Se hace el export default para que sea accesible desde el index.js
+ * * Para utilizar el store, hay que incluirlo como un plugin en el main.js
+ * * state - 
+ * * mutations - 
+ * * actions -
+ */
+export default createStore({
+
+    state:{
+        count: 1
+    }
+
+
+
+})
